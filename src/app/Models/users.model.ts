c9ed8a4455b32a00d50ba7802a24eb2bc2b61d93
malla sagar <mallasagar0101@gmail.com>
2024-03-10 23:55:39 -0700

    project setup

---
+++ src/app/Models/users.model.ts
@@ -0,0 +1,9 @@
+export interface UserModel{
+    username:string;
+    useraddress:string;
+    useremail:string;
+    userpassword:string;
+    usercontact:number;
+    usergender:string;
+    userrole:string;
+}
